f3383d1417c7bcda4207ae151a7db4f4a7083e4c
Murat Dursun <dursun.murat@fh-sw.de>
2022-06-08 13:13:22 +0200

    Test User

---
+++ src/Raumbuchungssystem/Objekte/User.java
@@ -0,0 +1,13 @@
+package src.Raumbuchungssystem.Objekte;
+/*
+@Author Murat Dursun
+ */
+public class User {
+
+    private String name = "";
+
+    private int id = 0;
+
+    private String email = "";
+
+}
