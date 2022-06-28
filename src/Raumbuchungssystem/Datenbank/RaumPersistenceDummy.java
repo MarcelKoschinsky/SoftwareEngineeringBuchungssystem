dd9dd52750c4173fb31d12f4a0aa4edf46087bbb
MarcelKoschinsky <marcel.koschinsky@web.de>
2022-06-28 19:56:53 +0200

    Test

--- src/Raumbuchungssystem/Datenbank/RaumPersistenceDummy.java
+++ src/Raumbuchungssystem/Datenbank/RaumPersistenceDummy.java
@@ -28,24 +28,4 @@ public class RaumPersistenceDummy {
 
 
 
-    int uhrzeit = ;
-    int tag;
-
-    int[][] kalender = {
-            {0, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-            {1, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-            {2, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-            {3, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-            {4, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-            {5, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-            {6, 2, 2, 4, 4, 2, 2, 4, 2, 2, 2, 2, 4, 2, 4, 2, 2, 2},
-
-
-    };
-
- if (kalender [tag][uhrzeit] = 2) {
-         System.out.println("Der Termin ist frei!");
-         } else {
-         System.out.println("Der Termin ist leider besetzt");
-         }
 
