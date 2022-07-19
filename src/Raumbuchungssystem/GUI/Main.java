50a723a7f9719f15b3baf469217ca394dc5c81d3
Murat-Dursun <95693248+Murat-Dursun@users.noreply.github.com>
2022-07-19 12:50:33 +0200

    Bei Knopfdruck wird nun etwas ausgegeben. Die Aufgabe muss noch die Methode bucheRaum ausführen.

--- src/Raumbuchungssystem/GUI/Main.java
+++ src/Raumbuchungssystem/GUI/Main.java
@@ -10,19 +10,29 @@ import src.Raumbuchungssystem.Logik.IRaumManager;
 import src.Raumbuchungssystem.Logik.RaumManager;
 import src.Raumbuchungssystem.Objekte.Raum;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.util.Scanner;
+import java.awt.event.*;
 import java.util.Set;
 
-public class Main {
+public class Main implements ActionListener{
 
     IRaumManager raumManager = new RaumManager();   //neuen RaumManager erstellt
 
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwortLabel;
+    private static JTextField passwordText;
+    private static JLabel raumNrLabel;
+    private static JTextField raumNrText;
+    private static JLabel uhrzeitLabel;
+    private static JTextField uhrzeitText;
+    private static JLabel wochentagLabel;
+    private static JTextField wochentagText;
+    private static JButton button1;
+    private static JLabel ergebnisLabel;
+
     public static void main (String[]args) {
+
         IRaumManager raumManager = new RaumManager();   //neuen RaumManager erstellt
 
         Set<Raum> raeume = raumManager.listeAlleRaeume();//erstelle eine liste aller Raeume
@@ -58,67 +68,72 @@ public class Main {
         panel.setLayout(null);
 
         //Benutzereingabe
-        JLabel userLabel = new JLabel("Benutzer");
+        userLabel = new JLabel("Benutzer");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField(20);
+        userText = new JTextField(20);
         userText.setBounds(100, 20, 165, 25);
         panel.add(userText);
 
         //Passworteingabe
-        JLabel passwordLabel = new JLabel("Passwort");
-        passwordLabel.setBounds(10, 50, 80, 25);
-        panel.add(passwordLabel);
+        passwortLabel = new JLabel("Passwort");
+        passwortLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwortLabel);
 
-        JTextField passwordText = new JPasswordField();
+        passwordText = new JPasswordField();
         passwordText.setBounds(100, 50, 165, 25);
         panel.add(passwordText);
 
         //Eingabe der RaumNr
-        JLabel raumNrLabel = new JLabel("Raum Nr.");
+        raumNrLabel = new JLabel("Raum Nr.");
         raumNrLabel.setBounds(10, 80, 80, 25);
         panel.add(raumNrLabel);
 
-        JTextField raumNrText = new JTextField(20);
+        raumNrText = new JTextField(20);
         raumNrText.setBounds(100, 80, 165, 25);
         panel.add(raumNrText);
 
+
         //Eingabe der Uhrzeit
-        JLabel uhrzeitLabel = new JLabel("Uhrzeit");
+        uhrzeitLabel = new JLabel("Uhrzeit");
         uhrzeitLabel.setBounds(10, 110, 80, 25);
         panel.add(uhrzeitLabel);
 
-        JTextField uhrzeitText = new JTextField(20);
+        uhrzeitText = new JTextField(20);
         uhrzeitText.setBounds(100, 110, 165, 25);
         panel.add(uhrzeitText);
 
         //Eingabe des Wochentages
-        JLabel wochentagLabel = new JLabel("Wochentag");
+        wochentagLabel = new JLabel("Wochentag");
         wochentagLabel.setBounds(10, 140, 80, 25);
         panel.add(wochentagLabel);
 
-        JTextField wochentagText = new JTextField(20);
+        wochentagText = new JTextField(20);
         wochentagText.setBounds(100, 140, 165, 25);
         panel.add(wochentagText);
 
         //Anmelden Knopf
-        JButton button1 = new JButton("Anmelden");
+        button1 = new JButton("Anmelden");
         button1.setBounds(10, 170, 100, 25);
         panel.add(button1);
+        button1.addActionListener(new Main());
 
-        ActionListener actionListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == button1){
-                   raumManager.bucheRaum(Integer.parseInt(raumNrText.getText()), Integer.parseInt(uhrzeitText.getText()), Integer.parseInt(wochentagText.getText()), userText.getText());
-                }
-            }
-        };
+        //Ergebnis Label
+        ergebnisLabel = new JLabel("");
+        ergebnisLabel.setBounds(10, 210, 200, 25);
+        panel.add(ergebnisLabel);
 
         frame.setVisible(true);
 
         //raumManager.bucheRaum(raumNr, uhrzeit, wochentag, name);
-
     }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ergebnisLabel.setText("Knopf wurde gedrueckt");
+    }
+
 }
